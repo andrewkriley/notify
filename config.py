cc1e5f2711ad9ddda2c30c2da37b764e0d55a495
Andrew Riley <andrew@andrewriley.info>
2017-04-08 19:51:02 +1000

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,15 @@
 #usr/bin/python
+
+#Configuration parameters for notify.py
+
+#Cisco Spark parameters
+ciscoSparkToken = ""
+ciscoSparkRoomId = ""
+
+#Click-a-Tell parameters
+clickatelUser = ""
+clickatelPass = ""
+
+#GMAIL
+gmailUser = ""
+gmailPass = ""
